9f4f8e835d5db36121e173aa87fc671fb2b13727
WeisgerberEstelle <89032759+WeisgerberEstelle@users.noreply.github.com>
2023-03-21 13:58:40 +0100

    quest java oop1

---
+++ Classroom.java
@@ -0,0 +1,8 @@
+public class Classroom {
+    public static void main(String[] args){
+        Wilder selena = new Wilder("Séléna", false );
+        Wilder jonas = new Wilder("Jonas", false);
+        System.out.println(selena.whoAmI());
+        System.out.println(jonas.whoAmI());
+    }
+}
